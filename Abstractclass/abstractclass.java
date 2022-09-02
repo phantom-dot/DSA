d41e96d24f508a91245f0b9ed52a8cd4b664da83
sahil <lohransahil@gmail.com>
2022-09-02 08:56:47 +0530

    abstractclass
    Signed-off-by: sahil <lohransahil@gmail.com>

--- Abstractclass/abstractclass.java
+++ Abstractclass/abstractclass.java
@@ -11,17 +11,23 @@ abstract class base{
         System.out.println("helloji");
     }
     abstract public void greet();
+    abstract public void greet2();
     //if we declare an abstract function then we must make that class abstract.
     //abstract class helps to define a standard for other classes
 }
 
 class child1 extends base {
-//if we extend an abstract a class then we must make the child class abstract or should define
+//if we extend an abstract a class then we must make the child class abstract or should define all the abstract functions of the base class
 //the abstract method of abstract class
 
     @Override
     public void greet() {
         System.out.println("hello ji");
+    }
+
+    @Override
+    public void greet2() {
+        System.out.println("greet2");
     }
 }
 class child2 extends base {
@@ -30,6 +36,11 @@ class child2 extends base {
     @Override
     public void greet() {
         System.out.println("su prbhaat");
+    }
+
+    @Override
+    public void greet2() {
+
     }
 }
 
